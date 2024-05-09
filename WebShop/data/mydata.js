f6331d3d0dc0ea9e4e8903ecbed9a1857b2c821e
Antonio Hohnjec <148785817+HohnjecAntonio@users.noreply.github.com>
2024-05-09 04:33:37 +0200

    Add files via upload

---
+++ WebShop/data/mydata.js
@@ -0,0 +1,227 @@
+const data = {
+    "website": "Odjeca",
+    "categories": [
+        { 
+            "name" : "Tenisice",
+            "image" : "/images/patike/patike1.jpg",
+            "products" : [
+                { 
+                    "name" : "Tenisice bijelo sive", "image" : "/images/patike/patike1.jpg"
+                },
+                { 
+                    "name" : "Tenisice planinarske", "image" : "/images/patike/patike2.jpg"
+                },                
+                { 
+                    "name" : "Tenisice zeleni converse", "image" : "/images/patike/patike3.jpg"
+                }, 
+                { 
+                    "name" : "Tenisice zelene moderna", "image" : "/images/patike/patike4.jpg"
+                }, 
+                { 
+                    "name" : "Tenisice converse crvene", "image" : "/images/patike/patike5.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Carape",
+            "image" : "/images/carape/carape1.jpg",
+            "products" : [
+                { 
+                    "name" : "Čarape sivo sa plavim obrubom", "image" : "/images/carape/carape1.jpg"
+                },
+                { 
+                    "name" : "Carape valoviti uzorak", "image" : "/images/carape/carape2.jpg"
+                },                
+                { 
+                    "name" : "Carape uzorak biljke", "image" : "/images/carape/carape3.jpg"
+                }, 
+                { 
+                    "name" : "Carape uzorak portret osobe", "image" : "/images/carape/carape4.jpg"
+                }, 
+                { 
+                    "name" : "Carape stopalice bijele", "image" : "/images/carape/carape5.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Cipele",
+            "image" : "/images/cipele/cipele1.jpg",
+            "products" : [
+                { 
+                    "name" : "Visoke žute cipele", "image" : "/images/cipele/cipele1.jpg"
+                },
+                { 
+                    "name" : "Svećane plave cipele(sjaj)", "image" : "/images/cipele/cipele2.jpg"
+                },                
+                { 
+                    "name" : "Tamno plave cipele", "image" : "/images/cipele/cipele3.jpg"
+                }, 
+                { 
+                    "name" : "Smeđe svećane cipele", "image" : "/images/cipele/cipele4.jpg"
+                }, 
+                { 
+                    "name" : "Croc cipele", "image" : "/images/cipele/cipele5.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Duge_majice",
+            "image" : "/images/duge_majice/dmajica1.jpg",
+            "products" : [
+                { 
+                    "name" : "Svjetlo siva majica", "image" : "/images/duge_majice/dmajica1.jpg"
+                },
+                { 
+                    "name" : "Majica s otvorenim ramenima", "image" : "/images/duge_majice/dmajica2.jpg"
+                },                
+                { 
+                    "name" : "Svjetlo smeđa izdužena majica", "image" : "/images/duge_majice/dmajica3.jpg"
+                }, 
+                { 
+                    "name" : "Bijela majica s ovratnikom", "image" : "/images/duge_majice/dmajica4.jpg"
+                }, 
+                { 
+                    "name" : "Crno bijela majica", "image" : "/images/duge_majice/dmajica5.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Hoodie",
+            "image" : "/images/hoodie/hoodie1.jpg",
+            "products" : [
+                { 
+                    "name" : "Svjetlo siva hoodie majica", "image" : "/images/hoodie/hoodie1.jpg"
+                },
+                { 
+                    "name" : "Vans crna hoodie majica", "image" : "/images/hoodie/hoodie2.jpg"
+                },                
+                { 
+                    "name" : "Simple crna hoodie majica", "image" : "/images/hoodie/hoodie3.jpg"
+                }, 
+                { 
+                    "name" : "Simple žuta hoodie majica", "image" : "/images/hoodie/hoodie4.jpg"
+                }, 
+                { 
+                    "name" : "Motivacijska zelena hoodie majica", "image" : "/images/hoodie/hoodie5.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Kratke_hlace",
+            "image" : "/images/kratke_hlace/shorts1.jpg",
+            "products" : [
+                { 
+                    "name" : "Kratke traper hlače s oderanim obrubom", "image" : "/images/kratke_hlace/shorts1.jpg"
+                },
+                { 
+                    "name" : "Kratke cyan traper hlače", "image" : "/images/kratke_hlace/shorts2.jpg"
+                },                
+                { 
+                    "name" : "Ultra kratke crne hlače", "image" : "/images/kratke_hlace/shorts3.jpg"
+                }, 
+                { 
+                    "name" : "New In kratke hlače", "image" : "/images/kratke_hlace/shorts4.jpg"
+                }, 
+                { 
+                    "name" : "Ultra kratke bijele traper hlače", "image" : "/images/kratke_hlace/shorts5.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Kratke_majice",
+            "image" : "/images/kratke_majice/kmajica1.jpg",
+            "products" : [
+                { 
+                    "name" : "Dont Smoke kratka majica", "image" : "/images/kratke_majice/kmajica1.jpg"
+                },
+                { 
+                    "name" : "One piece kratka majica", "image" : "/images/kratke_majice/kmajica2.jpg"
+                },                
+                { 
+                    "name" : "Simple bijela kratka majica", "image" : "/images/kratke_majice/kmajica3.jpg"
+                }, 
+                { 
+                    "name" : "Oakley kratka majica", "image" : "/images/kratke_majice/kmajica4.jpg"
+                }, 
+                { 
+                    "name" : "Motivacijska kratka majica", "image" : "/images/kratke_majice/kmajica5.jpg"
+                },
+                { 
+                    "name" : "Pamučna kratka majica", "image" : "/images/kratke_majice/kmajica6.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Papuce",
+            "image" : "/images/papuce/papuce1.jpg",
+            "products" : [
+                { 
+                    "name" : "Papuče I do", "image" : "/images/papuce/papuce1.jpg"
+                },
+                { 
+                    "name" : "Papuče crne moderna", "image" : "/images/papuce/papuce2.jpg"
+                },                
+                { 
+                    "name" : "Papuče japanke uzorak zebre", "image" : "/images/papuce/papuce3.jpg"
+                }, 
+                { 
+                    "name" : "Papuče japanke simple bijele", "image" : "/images/papuce/papuce4.jpg"
+                }, 
+                { 
+                    "name" : "Papuče Lock down krznene", "image" : "/images/papuce/papuce5.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Traperice",
+            "image" : "/images/traperice/traperice1.jpg",
+            "products" : [
+                { 
+                    "name" : "Regular plave traper hlače", "image" : "/images/traperice/traperice1.jpg"
+                },
+                { 
+                    "name" : "Slim oderane plave traper hlače", "image" : "/images/traperice/traperice2.jpg"
+                },                
+                { 
+                    "name" : "Slim podvez tamnoplave traper hlače", "image" : "/images/traperice/traperice3.jpg"
+                }, 
+                { 
+                    "name" : "Oderane regular plave traper hlače", "image" : "/images/traperice/traperice4.jpg"
+                }, 
+                { 
+                    "name" : "Slim podvez cyan traper hlače", "image" : "/images/traperice/traperice5.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Trenirke",
+            "image" : "/images/trenirke/trenirka1.jpg",
+            "products" : [
+                { 
+                    "name" : "Running fit trenirka", "image" : "/images/trenirke/trenirka1.jpg"
+                },
+                { 
+                    "name" : "Skye stye trenirka", "image" : "/images/trenirke/trenirka2.jpg"
+                },                
+                { 
+                    "name" : "Simple bijela pamučna trenirka", "image" : "/images/trenirke/trenirka3.jpg"
+                }, 
+                { 
+                    "name" : "Siva tension trenirka", "image" : "/images/trenirke/trenirka4.jpg"
+                }, 
+                { 
+                    "name" : "Crna sportska trenirka", "image" : "/images/trenirke/trenirka5.jpg"
+                }
+            ]
+        }
+        
+    ]
+}
+
+module.exports=data;
